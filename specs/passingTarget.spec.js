4a4b42d73369162a7391c479460088c20a07f88d
Christiano Milfont <cmilfont@gmail.com>
2012-05-22 14:25:42 -0300

    [close #4] fix bug

--- specs/passingTarget.spec.js
+++ specs/passingTarget.spec.js
@@ -1,7 +1,8 @@
 var linkify = function(url, target) {
 		return jQuery("<div>").html(url).linkify(target);
 	},
-	linkified  = "<a href=\"http://www.mifont.org\" target=\"_blank\">http://www.mifont.org</a>";
+	linkified  = "<a href=\"http://www.mifont.org\" target=\"_blank\">http://www.mifont.org</a>",
+	linkifiedNoProtocal  = "<a href=\"http://www.mifont.org\" target=\"_blank\">www.mifont.org</a>";
 
 describe("Linkify with Target", function() {
 
@@ -11,6 +12,10 @@ describe("Linkify with Target", function() {
 	
 	it('should target in link tag', function() {
 		expect( linkified ).toEqual( linkify( "http://www.mifont.org", "target='_blank'" ).html() );
+	});
+	
+	it("should target when there isn't protocol in URL", function() {
+		expect( linkifiedNoProtocal ).toEqual( linkify( "www.mifont.org", "target='_blank'" ).html() );
 	});
 	
 });
